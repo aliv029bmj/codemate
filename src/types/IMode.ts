e4e48bba1603b636d71e60c9257d71193613e772
Ali Novruz <elirehim68@gmail.com>
2025-05-29 08:08:55 +0400

    Add IMode interface

---
+++ src/types/IMode.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+
+/**
+ * Interface for all CodeMate modes
+ */
+export interface IMode {
+  /**
+   * The name of the mode
+   */
+  name: string;
+
+  /**
+   * The unique identifier for the mode
+   */
+  id: string;
+
+  /**
+   * The status bar item to display the mode information
+   */
+  statusBarItem: vscode.StatusBarItem;
+  
+  /**
+   * Activates the mode
+   * @param context The extension context
+   */
+  activate(context: vscode.ExtensionContext): void;
+  
+  /**
+   * Deactivates the mode
+   */
+  deactivate(): void;
+  
+  /**
+   * Updates the mode display based on cursor position
+   * @param line Current line number
+   * @param column Current column number
+   */
+  update(line: number, column: number): void;
+}
